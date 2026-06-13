6e28878fe1d9e493e4a34bd44782512da5607bc9
SATYAM SIVAM BARIK <satyamsivambarik@gmail.com>
2026-06-13 20:09:39 +0530

    Solved Group Anagrams using HashMap and Sorting

---
+++ Strings/GroupAnagrams.java
@@ -0,0 +1,31 @@
+INPUT
+  ["eat","tea","tan","ate","nat","bat"]
+OUTPUT
+  [["eat","tea","ate"],["tan","nat"],["bat"]]
+
+JAVA SOLUTION.....
+
+  import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+
+            String key = new String(chars);
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
