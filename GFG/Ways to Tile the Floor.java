7aa40532d2149fdd0770bc890944b3c9ad9ab616
SATYAM SIVAM BARIK <satyamsivambarik@gmail.com>
2026-06-27 23:51:18 +0530

    Solve  the question Ways to Tile the Floor
    Solve "Ways to Tile the Floor" using Dynamic Programming.
    
    - Implemented O(n) DP solution.
    - Used recurrence:
      dp[i] = dp[i-1]                  (i < m)
      dp[i] = dp[i-1] + 1              (i == m)
      dp[i] = dp[i-1] + dp[i-m]        (i > m)
    - Applied modulo 1e9+7 to handle large results.
    - Time Complexity: O(n)
    - Space Complexity: O(n)

---
+++ GFG/Ways to Tile the Floor.java
@@ -0,0 +1,20 @@
+class Solution {
+    static final int MOD = 1000000007;
+
+    public int countWays(int n, int m) {
+        long[] dp = new long[n + 1];
+        dp[0] = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (i < m) {
+                dp[i] = dp[i - 1];
+            } else if (i == m) {
+                dp[i] = (dp[i - 1] + 1) % MOD;
+            } else {
+                dp[i] = (dp[i - 1] + dp[i - m]) % MOD;
+            }
+        }
+
+        return (int) dp[n];
+    }
+}
